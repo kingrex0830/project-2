f7988f4b458389fdc288c9dcc719c2777817b0a0
Melanie Wang <melaniiewang@hotmail.com>
2023-06-12 21:02:07 -0400

    fixed up signup connection and created branch

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const signupRoutes = require('./signupRoutes');
+// const aboutRoutes = require('./about');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/signup', signupRoutes);
+// router.use('/about', aboutRoutes);
 
 module.exports = router;
