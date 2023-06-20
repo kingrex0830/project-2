7df1cdf086c2cb14f3e7d8bb414ce614c1f2aeac
MHMD101 <atrach.muhammad@gmail.com>
2023-06-20 17:04:26 -0400

    Updated the js for the main dashboard and made sure all functionality works. Now we can: Add grades, view Grades, Update grades, and delete grades!

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -337,6 +337,275 @@ async function handleUpdateButtonClick() {
   }
 }
 
+// NOTE
+// GO OVer it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// search for student to get their grades
+async function handleStudentSearch() {
+  const studentIdInput = document.querySelector('#student-name');
+  const studentId = studentIdInput.value.trim();
+
+  // Fetch the grades data for the specific student
+  const response = await fetch(`/api/grades?student_id=${studentId}`);
+  const grades = await response.json();
+
+  if (Array.isArray(grades) && grades.length > 0) {
+    // Sort the grades array based on student name
+    const sortedGrades = grades.sort((a, b) => {
+      const studentA =
+        a.student && a.student.first_name && a.student.last_name
+          ? `${a.student.first_name} ${a.student.last_name}`.toLowerCase()
+          : '';
+      const studentB =
+        b.student && b.student.first_name && b.student.last_name
+          ? `${b.student.first_name} ${b.student.last_name}`.toLowerCase()
+          : '';
+      return studentA.localeCompare(studentB);
+    });
+
+    // Render the grades table
+    renderGradesTable(sortedGrades);
+  } else {
+    // Handle the case when no grades are found for the student
+    renderGradesTable([]);
+  }
+}
+
+function renderGradesTable(grades) {
+  const studentIdInput = document.querySelector('#student-name');
+  const studentId = studentIdInput.value.trim();
+
+  const container = document.querySelector('#grades-table-container');
+
+  if (container && studentId) {
+    if (grades.length > 0) {
+      // Create the table structure
+      const table = document.createElement('table');
+      const thead = document.createElement('thead');
+      const tbody = document.createElement('tbody');
+
+      // Add table headers
+      const headers = ['Student Name', 'Course Name', 'Grade', 'Actions'];
+      const headerRow = document.createElement('tr');
+      headers.forEach((header) => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+      });
+      thead.appendChild(headerRow);
+
+      // Add table rows for each grade
+      grades.forEach((grade) => {
+        const { id, student, course, value } = grade;
+
+        const row = document.createElement('tr');
+        const studentNameCell = document.createElement('td');
+        studentNameCell.textContent = student
+          ? `${student.first_name} ${student.last_name}`
+          : 'N/A';
+        const courseNameCell = document.createElement('td');
+        const courseLink = document.createElement('a');
+        courseLink.href = `/course/${course ? course.course_id : ''}`;
+        courseLink.textContent = course ? course.course_name : 'N/A';
+        courseNameCell.appendChild(courseLink);
+        const gradeCell = document.createElement('td');
+        gradeCell.textContent = value;
+        const actionsCell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('btn', 'btn-sm', 'btn-danger');
+        deleteButton.value = grade.id;
+        deleteButton.addEventListener('click', () => {
+          // Call a function to handle the delete action
+          handleGradeDelete(id);
+        });
+        actionsCell.appendChild(deleteButton);
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.classList.add('btn', 'btn-sm', 'btn-primary');
+        editButton.value = grade.id;
+        editButton.addEventListener('click', () => {
+          // Call a function to handle the edit action
+          handleGradeEdit(id, row, value);
+        });
+        actionsCell.appendChild(editButton);
+
+        row.appendChild(studentNameCell);
+        row.appendChild(courseNameCell);
+        row.appendChild(gradeCell);
+        row.appendChild(actionsCell);
+
+        tbody.appendChild(row);
+      });
+
+      // Add the table to the container
+      table.appendChild(thead);
+      table.appendChild(tbody);
+      container.innerHTML = ''; // Clear previous content
+      container.appendChild(table);
+    } else {
+      // Handle the case when no grades are found
+      container.innerHTML = '<p>No grades found.</p>';
+    }
+  } else {
+    const container = document.querySelector('#grades-table-container');
+    container.textContent = '';
+  }
+}
+
+async function handleGradeDelete(gradeId) {
+  // Perform the delete operation using the gradeId
+  const deleteGradeResponse = await fetch(`/api/grades/${gradeId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (deleteGradeResponse.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete grade');
+  }
+}
+
+async function handleGradeEdit(gradeId, row) {
+  const cells = row.getElementsByTagName('td');
+
+  const gradeCell = cells[2];
+  const gradeValue = gradeCell.textContent;
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.value = gradeValue;
+
+  gradeCell.textContent = '';
+  gradeCell.appendChild(input);
+
+  const saveButton = document.createElement('button');
+  saveButton.textContent = 'Save';
+  saveButton.classList.add('btn', 'btn-sm', 'btn-success');
+  saveButton.addEventListener('click', () => {
+    handleGradeSave(gradeId, row);
+  });
+
+  const actionsCell = cells[cells.length - 1];
+  actionsCell.textContent = '';
+  actionsCell.appendChild(saveButton);
+}
+
+async function handleGradeSave(gradeId, row) {
+  const inputs = row.getElementsByTagName('input');
+  const gradeValueInput = inputs[0];
+  const gradeValue = gradeValueInput.value || gradeValueInput.placeholder;
+
+  try {
+    const updateGradeResponse = await fetch(`/api/grades/${gradeId}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        value: gradeValue,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!updateGradeResponse.ok) {
+      throw new Error('Failed to edit grade');
+    } else {
+      document.location.replace('/dashboard');
+    }
+
+    // Not working for some reason: Give u the catch error if added
+    // const container = document.querySelector('#grades-table-container');
+    // container.appendChild(table);
+  } catch (error) {
+    console.error(error);
+    alert('Failed to edit grade');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const studentSelect = document.querySelector('#student-select');
+  const courseSelect = document.querySelector('#course-select');
+  const gradeValueInput = document.querySelector('#grade-value');
+  const addGradeButton = document.querySelector('#add-grade-button');
+
+  addGradeButton.addEventListener('click', handleAddGrade);
+
+  async function handleAddGrade() {
+    const selectedStudent = studentSelect.value;
+    const selectedCourse = courseSelect.value;
+    const gradeValue = gradeValueInput.value.trim();
+
+    if (!selectedStudent || !selectedCourse || !gradeValue) {
+      alert('Please select a student, course, and enter a grade value');
+      return;
+    }
+
+    try {
+      const addGradeResponse = await fetch('/api/grades', {
+        method: 'POST',
+        body: JSON.stringify({
+          student_id: selectedStudent,
+          course_id: selectedCourse,
+          value: gradeValue,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (addGradeResponse.ok) {
+        // go to updated page
+        document.location.replace('/dashboard');
+      } else {
+        throw new Error('Failed to add grade');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Failed to add grade');
+    }
+  }
+
+  async function fetchStudentsAndCourses() {
+    try {
+      const studentsResponse = await fetch('/api/students');
+      const students = await studentsResponse.json();
+
+      students.forEach((student) => {
+        const option = document.createElement('option');
+        option.value = student.id;
+        option.textContent = `${student.first_name} ${student.last_name}`;
+        studentSelect.appendChild(option);
+      });
+
+      const coursesResponse = await fetch('/api/courses');
+      const courses = await coursesResponse.json();
+
+      courses.forEach((course) => {
+        const option = document.createElement('option');
+        option.value = course.id;
+        option.textContent = course.course_name;
+        courseSelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error(error);
+      alert('Failed to fetch students and courses');
+    }
+  }
+
+  // Fetch students and courses data when the page loads
+  
+  'DOMContentLoaded', () => {
+  fetchStudentsAndCourses();
+  };
+});
+
+// Event listener for student search button
+const searchBtn = document.querySelector('#student-search-btn');
+searchBtn.addEventListener('click', handleStudentSearch);
+
 // All functions called here:
 fetchDepartments();
 
